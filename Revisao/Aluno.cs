02de95ad83790a86fd45da860ce54d518dfb43a3
Karla Azambuja <karlavf28@gmail.com>
2021-04-05 14:05:35 -0300

    Criando um projeto de revisao e o menu de opções

---
+++ Revisao/Aluno.cs
@@ -0,0 +1,10 @@
+namespace Revisao
+{
+
+    //Poderia ser cirada uma classe, mas vamos usar struct para fins didáticos
+    public struct Aluno
+    {
+        public string  Nome { get; set; }
+        public decimal  Nota { get; set; }
+    }
+}
---
+++ Revisao/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno[] alunos = new Aluno[5];
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        // TODO
+                        break;
+
+                    case "2":
+                        //TODO
+                        break;
+
+                    case "3":
+                        //TODO
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+               opcaoUsuario = ObterOpcaoUsuario();
+
+               
+
+
+            }
+
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Adicione um aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+
+            string opcaoUsuario = Console.ReadLine();
+
+            return opcaoUsuario;
+        }
+    }
+}
