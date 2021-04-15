42071c75d00c584955d503a4f093de910f5fba80
Karla Azambuja <karlavf28@gmail.com>
2021-04-15 17:35:23 -0300

    Projeto MVC com EntitiyFramework

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1401; Database=Cursomvc;User=SA; Password=<Numsey@Password>");
+
+         }
+
+    }
+}
